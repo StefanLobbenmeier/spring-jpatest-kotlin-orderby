7086cbea3f42dbaa34871becd08d1c4c354e8427
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2022-11-17 12:06:57 +0100

    Assert on retrievedStudent instead

--- src/test/kotlin/com/example/springjpatestkotlinorderby/SpringJpatestKotlinOrderbyApplicationTests.kt
+++ src/test/kotlin/com/example/springjpatestkotlinorderby/SpringJpatestKotlinOrderbyApplicationTests.kt
@@ -1,13 +1,14 @@
 package com.example.springjpatestkotlinorderby
 
+import assertk.assertAll
 import assertk.assertThat
-import assertk.assertions.isDataClassEqualTo
 import assertk.assertions.isEqualTo
 import java.math.BigDecimal
 import java.time.LocalDate
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 class SpringJpatestKotlinOrderbyApplicationTests {
@@ -20,6 +21,7 @@ class SpringJpatestKotlinOrderbyApplicationTests {
     }
 
     @Test
+    @Transactional
     fun gradesAreRetrievedInOrder() {
         val student = studentRepository.save(Student(name = "John"))
 
@@ -28,11 +30,16 @@ class SpringJpatestKotlinOrderbyApplicationTests {
         student.grades.add(Grade(date = LocalDate.of(2022, 1, 1), value = gradeValue))
         student.grades.add(Grade(date = LocalDate.of(2020, 1, 1), value = gradeValue))
 
-        val studentWithGrades = studentRepository.save(student)
+        val savedStudent = studentRepository.save(student)
 
-        assertThat(studentWithGrades.grades[0].date.year).isEqualTo(2022)
-        assertThat(studentWithGrades.grades[1].date.year).isEqualTo(2021)
-        assertThat(studentWithGrades.grades[2].date.year).isEqualTo(2020)
+        val retrievedStudent = studentRepository.getReferenceById(savedStudent.id!!)
+
+        assertAll {
+            assertThat(retrievedStudent.grades[0].date.year).isEqualTo(2022)
+            assertThat(retrievedStudent.grades[1].date.year).isEqualTo(2021)
+            assertThat(retrievedStudent.grades[2].date.year).isEqualTo(2020)
+
+        }
     }
 
 }
