dffc2d5155692eabe1f69fea51ceea18db44f9a6
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2022-11-17 11:12:02 +0100

    Spring initializer

---
+++ src/main/kotlin/com/example/springjpatestkotlinorderby/SpringJpatestKotlinOrderbyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springjpatestkotlinorderby
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpatestKotlinOrderbyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJpatestKotlinOrderbyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springjpatestkotlinorderby/SpringJpatestKotlinOrderbyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springjpatestkotlinorderby
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJpatestKotlinOrderbyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
