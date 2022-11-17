dfbc5427a4c91e84ac60fa04ec3705d9118c3839
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2022-11-17 12:15:33 +0100

    Split test on saved entity and retrieved entity

--- src/test/kotlin/com/example/springjpatestkotlinorderby/SpringJpatestKotlinOrderbyApplicationTests.kt
+++ src/test/kotlin/com/example/springjpatestkotlinorderby/SpringJpatestKotlinOrderbyApplicationTests.kt
@@ -21,6 +21,24 @@ class SpringJpatestKotlinOrderbyApplicationTests {
     }
 
     @Test
+    fun gradesAreInOrderAfterSave() {
+        val student = studentRepository.save(Student(name = "John"))
+
+        val gradeValue = BigDecimal.valueOf(50)
+        student.grades.add(Grade(date = LocalDate.of(2021, 1, 1), value = gradeValue))
+        student.grades.add(Grade(date = LocalDate.of(2022, 1, 1), value = gradeValue))
+        student.grades.add(Grade(date = LocalDate.of(2020, 1, 1), value = gradeValue))
+
+        val savedStudent = studentRepository.save(student)
+
+        assertAll {
+            assertThat(savedStudent.grades[0].date.year).isEqualTo(2022)
+            assertThat(savedStudent.grades[1].date.year).isEqualTo(2021)
+            assertThat(savedStudent.grades[2].date.year).isEqualTo(2020)
+        }
+    }
+
+    @Test
     @Transactional
     fun gradesAreRetrievedInOrder() {
         val student = studentRepository.save(Student(name = "John"))
@@ -38,7 +56,6 @@ class SpringJpatestKotlinOrderbyApplicationTests {
             assertThat(retrievedStudent.grades[0].date.year).isEqualTo(2022)
             assertThat(retrievedStudent.grades[1].date.year).isEqualTo(2021)
             assertThat(retrievedStudent.grades[2].date.year).isEqualTo(2020)
-
         }
     }
 
